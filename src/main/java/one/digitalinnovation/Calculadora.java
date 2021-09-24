608078defd6c49166d77f7f3f60d03485fc63089
Ricky Bustillos <henrique.ssbl@hotmail.com>
2021-09-24 17:38:54 -0300

    Desenvolvido: método somar
    Desenvolvido o método somar na classe Calculadora

---
+++ src/main/java/one/digitalinnovation/Calculadora.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation;
+
+public class Calculadora {
+
+    public int somar(String expressao) {
+        int soma = 0;
+        for(String valor : expressao.split("\\+")) {
+            soma += Integer.valueOf(valor);
+        }
+
+        System.out.println(soma);
+        return soma;
+    }
+}
